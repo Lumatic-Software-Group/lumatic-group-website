45422b024b1da35b38042d433b9745e1faa02d14
Abbasian.dev <mam-1371@outlook.com>
2026-03-19 21:33:43 +0330

    chore: update .gitignore to exclude Next.js dev artifacts and fix hydration warning

--- docs/dev/static/chunks/Documents_web_front_abbasian_dev_lumatic-group-website_app_page_tsx_f396d341._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/Documents_web_front_abbasian_dev_lumatic-group-website_components_f484e802._.js",
-  "static/chunks/eada9_next_4b81d693._.js",
-  "static/chunks/Documents_web_front_abbasian_dev_lumatic-group-website_components_4abb89f4._.css"
-],
-    source: "dynamic"
-});
