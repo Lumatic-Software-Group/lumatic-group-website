9f26015c93ce651f63cbe46da4231a30b69f9582
Abbasian.dev <mam-1371@outlook.com>
2026-07-01 23:54:53 +0330

    refactor(global): Update global CSS and layout structure

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://lumaticgroup.info/',
-      lastModified: new Date('2026-03-19'),
+      lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1.0,
     },
